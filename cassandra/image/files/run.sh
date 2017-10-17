04d4d43a1aa85f822db1a7d4497a63b118be2d91
igamanyu <ivan_gamanyuk@optum.com>
2017-10-17 13:08:27 -0500

    security

--- cassandra/image/files/run.sh
+++ cassandra/image/files/run.sh
@@ -173,5 +173,8 @@ fi
 
 export CLASSPATH=/kubernetes-cassandra.jar
 
-#su cassandra -c "$CASSANDRA_HOME/bin/cassandra -f"
-$CASSANDRA_HOME/bin/cassandra -f
+whoami
+
+su cassandra -c "$CASSANDRA_HOME/bin/cassandra -f"
+
+#$CASSANDRA_HOME/bin/cassandra -f
