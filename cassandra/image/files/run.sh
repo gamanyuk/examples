1195d89cd3b8570dcad624e28487aa2517af996e
igamanyu <ivan_gamanyuk@optum.com>
2017-10-18 13:34:55 -0500

    seeds

--- cassandra/image/files/run.sh
+++ cassandra/image/files/run.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+echo CASSANDRA_SEEDS0 ${CASSANDRA_SEEDS}
+
 set -e
 CASSANDRA_CONF_DIR=/etc/cassandra
 CASSANDRA_CFG=$CASSANDRA_CONF_DIR/cassandra.yaml
